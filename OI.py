5d17402b909a4c5327320e3f82fe9d6f610097b1
mcrobr <47095535+mcrobr@users.noreply.github.com>
2019-03-20 15:44:31 -0500

    Add files via upload

--- OI.py
+++ OI.py
@@ -11,10 +11,19 @@ ib.connect('127.0.0.1', 4001, clientId=2)
 tickList = ['TSLA','PLNT','SNBR','DATA','TEAM','CRM','RUN','FIZZ','YETI','AER','FWONK','SPY','EEM']
 def getVolume():
     contract = Option('TSLA' , '20190322', 250, 'P', 'SMART')
-    hData = ib.reqHistoricalData(contract, '','60 S','1 day','TRADES',True)
-    print(hData)
-
-getVolume()
+    hData = ib.reqHistoricalData(contract, endDateTime='', durationStr='10 D',
+        barSizeSetting='30 mins', whatToShow='TRADES', useRTH=True)
+    dfExp = pd.DataFrame(columns = ['ticker','expiry','strike','type','bartime','volume','open', 'high','low','close'])
+    i = 0
+    for x in hData:
+        dfExp.loc[i] = [contract.symbol, contract.lastTradeDateOrContractMonth, contract.strike, contract.right, x.date, x.volume, x.open, x.high, x.low, x.close]
+        i = i+1
+        print(contract.symbol, contract.lastTradeDateOrContractMonth, contract.strike, contract.right, x.date, x.volume, x.open, x.high, x.low, x.close)
+    dfExp.to_csv('Ticks.csv')
+            
+####    print(hData[0].date,hData[1].date,hData[2].date,hData[3].date,hData[4].date,hData[5].date,hData[6].date,hData[7].date,hData[8].date,hData[9].date,hData[10].date,hData[11].date,hData[12].date)
+####    print(hData[0].volume,hData[1].volume,hData[2].volume,hData[3].volume,hData[4].volume,hData[5].volume,hData[6].volume,hData[7].volume,hData[8].volume,hData[9].volume,hData[10].volume,hData[11].volume,hData[12].volume)
+##getVolume()
 
 
 
@@ -26,7 +35,6 @@ def getOI(tickList):
         contract = Stock(x, 'SMART', 'USD')
         got = ib.reqMktData(contract, "100,101")
         ib.sleep(5)
-        print(got)
         print("Ticker:",contract.symbol)
         print("Put Volume:",got.putVolume)
         print("Put OI:",got.putOpenInterest)
