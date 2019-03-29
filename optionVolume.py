0579ecb85a82573bd55cbc815ffcf82395b303bf
mcrobr <47095535+mcrobr@users.noreply.github.com>
2019-03-29 14:44:19 -0500

    Add files via upload

--- optionVolume.py
+++ optionVolume.py
@@ -7,7 +7,7 @@ import os
 ib = IB()
 ib.connect('127.0.0.1', 4001, clientId=2)
 
-ticker = 'PLNT'
+ticker = 'TSLA'
 
 def getVolume(contract):
     hData = ib.reqHistoricalData(contract, endDateTime='', durationStr='2 D',
@@ -18,43 +18,38 @@ def getVolume(contract):
         dfExp.loc[i] = [contract.symbol, contract.lastTradeDateOrContractMonth, contract.strike, contract.right, x.date, x.volume, x.open, x.high, x.low, x.close]
         i = i+1
         print(contract.symbol, contract.lastTradeDateOrContractMonth, contract.strike, contract.right, x.date, x.volume, x.open, x.high, x.low, x.close)
-    if os.path.isfile('testTicks.csv') == True:
-        dfExp.to_csv('testTicks.csv', mode='a', header=False)
+    if os.path.isfile('testTicks5.csv') == True:
+        dfExp.to_csv('testTicks5.csv', mode='a', header=False)
     else:
-        dfExp.to_csv('testTicks.csv')  
+        dfExp.to_csv('testTicks5.csv')  
 
 def getStrDate(ticker):
     conId = ib.reqContractDetails(Stock(ticker))[0].contract.conId
     optparams = ib.reqSecDefOptParams(ticker,'','STK', conId)
 
-
-    i = 0
-    for x in optparams:
-        if x.exchange == 'SMART':
-            smartCell = i
-        else:
-            i = i + 1
-
-    expDates = optparams[smartCell].expirations
-    strikes = optparams[smartCell].strikes
-
-    returnFun = [expDates, strikes]
-    return returnFun
-
-dates = list(getStrDate(ticker)[0])
-strikes = list(getStrDate(ticker)[1])
-cList = []
-
-j=0
-while j < len(strikes):
-    i=0
-    while i < len(dates):
-        cList.append(Option(ticker,dates[i],strikes[j],'P','SMART'))
-        i = i+1
-    j = j+1
+    global cList
+    cList = []
     
-for x in cList:
-    getVolume(x)
+
+    k = 0
+    for x in optparams:
+        expDates = list(optparams[k].expirations)
+        strikes = list(optparams[k].strikes)
+        exchange = optparams[k].exchange
+        j=0
+        while j < len(strikes):
+            i=0
+            while i < len(expDates):
+                cList.append(Option(ticker,expDates[i],strikes[j],'P',exchange))
+                i = i+1
+            j = j+1
+        k = k+1
+
+
+getStrDate(ticker)
+print(len(cList))
+##for x in cList:
+##    getVolume(x)
 
 
 
